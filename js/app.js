7bb2592db0fa493a1119f85cfa8749dfc7290ef7
Julio <juliomoreno@gmail.com>
2022-03-31 20:36:52 +0200

    First branch commit with examples

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let nav = document.querySelector('nav');
+console.log(nav);
+let active = document.querySelector('.active');
+console.log(active);
+let paragraph = document.querySelector('#first');
+console.log(paragraph);
+let headersAndParagraphs = document.querySelectorAll('h1,p');
+console.log(headersAndParagraphs[0].innerText);
