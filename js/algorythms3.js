7be01b6787c5ee24b2f280952c5fc45ba1e86372
Julio <juliomoreno@gmail.com>
2022-04-01 18:48:33 +0200

    Several fixes to the first 3 exercises

--- js/algorythms3.js
+++ js/algorythms3.js
@@ -5,23 +5,16 @@
 const exercise1 = () => {
 
   let minLim = parseInt(prompt(`Please insert the minimum numerical limit`));
-
   let maxLim = parseInt(prompt(`Please insert the maximum numerical limit`));
 
   let randNums = Array.from(
-
     { length: 20 },
-
     () => Math.floor(Math.random() * (maxLim - minLim + 1)) + minLim
-
   );
 
   console.log(
-
     `min value: ${Math.min(...randNums)} \nmax value: ${Math.max(...randNums)}`
-
   );
-
 };
 
 exercise1();
@@ -30,148 +23,114 @@ exercise1();
 
 // EXCERSIZE 02
 
-// I really don't know where to put the function in this one
-
 /*
-
-const exercise2 = () => {
-
-if (new Date().getHours() < 12) {
-
-  alert("Buenos días");
-
-} else if (new Date().getHours() < 21) {
-
-  alert("Buenas tardes");
-
-} else {
-
-  alert("Buenas noches");
-
+const exercise2 = () => {*/
+/*
+new Date().getHours() >= 6 && new Date().getHours() < 12
+  ? alert(`Buenos días`)
+  : new Date().getHours() >= 12 && new Date().getHours() < 21
+  ? alert(`Buenas tardes`)
+  : alert(`Buenas noches`);
+*/
+/*
+  if (new Date().getHours() < 12) {
+    alert("Buenos días");
+  } else if (new Date().getHours() < 21) {
+    alert("Buenas tardes");
+  } else {
+    alert("Buenas noches");
+  }
 };
 
 exercise2();
-
 */
 
-/*
-
 // EXCERSIZE 03
-
-// const exercise3 = () => {
-
-// function randomHexColor() {
-
-//     color = "#";
-
-//     const colorCodes = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F",];
-
-//     for (let j = 0; j < 6; j++) {
-
-//       let index = Math.floor(Math.random() * colorCodes.length);
-
-//       color += colorCodes[index];
-
-//     }
-
-//     return color;
-
-//   }
-
-//   let color;
-
-//   for (let i = 0; i < 10; i++) {
-
-//     randomHexColor();
-
-//     console.log(`%cHex code for color number ${i+1}: ${color}`, `color:${color}`);
-
-//   };
-
-exercise3();*/
-
 // Shorter method hex code generation. 16777215 is the maximum value for RGB color white. But with the other method I get to use a function.
+/*
+const exercise3 = () => {
+  function generateRandomColor() {
+    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    if (randomColor.length != 7) {
+      // In any case, the color code is invalid
+      randomColor = generateRandomColor();
+    }
+    return randomColor; // The random color will be freshly served
+  }
 
-// const randomColor = Math.floor(Math.random()*16777215).toString(16); investigar o preguntar
+  for (let i = 0, hexColor; i < 10; i++) {
+    console.log(`%cHello World!`, `color:${generateRandomColor()}`);
+  }
+};
+exercise3();*/
+/*
+const exercise3 = () => {
+  function randomHexColor() {
+    color = "#";
+    const colorCodes = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    for (let j = 0; j < 6; j++) {
+      let index = Math.floor(Math.random() * colorCodes.length);
+      color += colorCodes[index];
+    }
+    return color;
+  }
+  let color;
+  for (let i = 0; i < 10; i++) {
+    randomHexColor();
+    console.log(
+      `%cHex code for color number ${i + 1}: ${color}`,
+      `color:${color}`
+    );
+  }
+};
+exercise3();
+*/
 
 // EXCERSIZE 04
-
 /*
-
 const exercise4 = () => {
-
 const idChars = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"];
-
 let userId = prompt("Please enter your DNI to validate");
-
 let idSum="";
-
 for (let i = 0; i < userId.length-1; i++) {
-
   idSum += userId.charAt(i);
-
 }
-
 if (userId == idSum + (idChars[parseInt(idSum)%23])) {
-
   console.log(`Valid DNI`);
-
 } else {
-
   console.log(`The entered data is wrong`);
-
+};
+exercise4();
+*/
+// EXCERSIZE 05
+/*
+const exercise5 = () => {
+  //const letters = ["B","C","D","F","G","H","J","K","L","M","N","P","R","S","T","V","W","X","Y","Z"];
+  let howMany = parseInt(prompt("How many number plates do you want?"));
+  let arrOfPlates = [];
+  function genPlates() {
+    let plateNum = [];
+    let index;
+    do {
+      index = Math.floor(Math.random() * 10);
+      plateNum.push(index);
+    } while (plateNum.length < 4);
+    do {
+      index = letters[Math.floor(Math.random() * letters.length)];
+      plateNum.push(index);
+    } while (plateNum.length < 7);
+    arrOfPlates.push(plateNum.join(""));
+  }
+  do {
+    genPlates();
+  } while (arrOfPlates.length < howMany);
+  console.log(arrOfPlates);
 };
 
-exercise4();*/
-
-// EXCERSIZE 05
-
-/*const exercise5 = () => {
-
-const letters = ["B","C","D","F","G","H","J","K","L","M","N","P","R","S","T","V","W","X","Y","Z"];
-
-let howMany = parseInt(prompt("How many number plates do you want?"));
-
-let arrOfPlates = [];
-
-function genPlates() {
-
-  let plateNum = [];
-
-  let index;
-
-  do {
-
-    index = Math.floor(Math.random() * 10);
-
-    plateNum.push(index);
-
-  } while (plateNum.length < 4);
-
-  do {
-
-    index = letters[Math.floor(Math.random() * letters.length)];
-
-    plateNum.push(index);
-
-  } while (plateNum.length < 7);
-
-  arrOfPlates.push(plateNum.join(""));
-
-}
-
-do {
-
-  genPlates();
-
-} while (arrOfPlates.length < howMany);
-
-console.log(arrOfPlates);};
-
-exercise5();*/
-
+exercise5();
+*/
 // EXCERSIZE 06
-
+/*
 const exercise6 = () => {
   const randQuote = () => {
     const quotes = Array.of(
@@ -304,24 +263,26 @@ const exercise6 = () => {
       )
     );
     const randNums = () => Math.floor(Math.random() * quotes.length);
-    console.log(quotes[randNums()]); //preguntar si se puede referenciar un indice de un array dentro de un array sin guardarlo en variable. Por ejemplo, mostrar solo los valores de la cita en sí y el personaje.
+    console.log(quotes[randNums()]); preguntar si se puede referenciar un indice de un array dentro de un array sin guardarlo en variable. Por ejemplo, mostrar solo los valores de la cita en sí y el personaje.
+
+    console.log(quotes[randNums()][2]); para sacar del arrayº interior, en este caso el actor
   };
 
-  // This method I found through a search engine solves my problems and I mostly understand it,
-  // I DID NOT WRITE THIS, I ONLY MODIFIED IT
+   This method I found through a search engine solves my problems and I mostly understand it,
+   I DID NOT WRITE THIS, I ONLY MODIFIED IT
 
-  let timerId = setInterval(() => randQuote(), 10000); // repeat with the interval of 10 seconds
+  let timerId = setInterval(() => randQuote(), 10000);  repeat with the interval of 10 seconds
   setTimeout(() => {
     clearInterval(timerId);
     randQuote();
-  }, 120000); // after 120 seconds stop
+  }, 120000);  after 120 seconds stop
 
-  //window.setInterval(randQuote, 10000); //se ejecuta (en este caso la funcion randQuote) continuamente cada 10 segundos. Incluso la primera ejecución ocurre tras 10 segundos de espera
-  //window.setTimeout(randQuote, 10000); //solo se ejecuta una vez tras esperar 10 segundos
+  window.setInterval(randQuote, 10000); se ejecuta (en este caso la funcion randQuote) continuamente cada 10 segundos. Incluso la primera ejecución ocurre tras 10 segundos de espera
+  window.setTimeout(randQuote, 10000); solo se ejecuta una vez tras esperar 10 segundos
 };
 
 exercise6();
-
+*/
 // EXCERSIZE 07
 /*
 const exercise7 = () => {
@@ -343,11 +304,12 @@ const exercise7 = () => {
 exercise7();
 */
 // EXCERSIZE 08
+/*
+DO WHILE ES IDONEO
 
-//DO WHILE ES IDONEO
-
-//guardar cartas en array y comparar si es mayor o menor la posicion en el array, no el valor en si.
-
+guardar cartas en array y comparar si es mayor o menor la posicion en el array, no el valor en si.
+*/
 // EXCERSIZE 09
-
-//pasar todo a uppercase -> rueda de alfabeto -> de las Z si avanza 3 posiciones seria la D
+/*
+pasar todo a uppercase -> rueda de alfabeto -> de las Z si avanza 3 posiciones seria la D
+*/
